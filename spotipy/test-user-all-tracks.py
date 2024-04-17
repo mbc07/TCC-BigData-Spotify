edaf107b14a18abe9349a2621069d8ab229db951
Mateus B. Cassiano <mbc07@live.com>
2024-04-17 13:44:46 -0400

    Salva resultados parciais

--- spotipy/test-user-all-tracks.py
+++ spotipy/test-user-all-tracks.py
@@ -8,8 +8,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
 # Configure as credenciais
-client_id = '066e8b7fdb554727b6cd9bc2b10551a5'
-client_secret = '3b08c73a70714e35a414fcfe3699b53f'
+client_id = '0dba842d359e48b9be6523154f3df372'
+client_secret = '8032d41cbfa14674ac9d45ac0a823331'
 scope = "playlist-read-private,playlist-read-collaborative,user-follow-read,user-top-read,user-read-recently-played,user-library-read"
 redirect_uri = "https://example.com/callback"
 
@@ -18,88 +18,99 @@ client_credentials_manager = SpotifyClientCredentials(client_id=client_id, clien
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, auth_manager=SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
 
 sent_requests = 0
+
+# Obtém todos os artistas seguidos
 artists = []
-albums = []
-album_tracks = []
-track_ids = []
-tracks = []
-
-# Obter todos os artistas seguidos
-results = sp.current_user_followed_artists(limit=50)
-sent_requests += 1
-artists.extend(results['artists']['items'])
-
-while True:
-    print("Obtendo artistas...")
+with open('artists.json', 'w') as fp:
+    results = sp.current_user_followed_artists(limit=50)
+    sent_requests += 1
     artists.extend(results['artists']['items'])
-    if results['artists']['next']:
-        if sent_requests > 150:
-            sent_requests = 0
-            print('Cooldown')
-            time.sleep(30)
 
-        results = sp.next(results['artists'])
-        sent_requests += 1
-    else:
-        break
+    while True:
+        print("Obtendo artistas...")
+        artists.extend(results['artists']['items'])
+        json.dump(results['artists']['items'], fp)   # Salva resultado parcial
+        if results['artists']['next']:
+            if sent_requests > 200:
+                sent_requests = 0
+                print('Cooldown')
+                time.sleep(30)
 
-# Salva detalhes dos artistas seguidos
+            results = sp.next(results['artists'])
+            sent_requests += 1
+        else:
+            break
+            
+# Subtitui resultados parciais com resultados finais
 with open('artists.json', 'w') as fp:
     json.dump(artists, fp)
 
-for artist in artists:
-    print("Obtendo álbuns...")
-    results = sp.artist_albums(artist['id'], album_type='album', limit=50)
-    sent_requests += 1
-    while True:
-        albums.extend(results['items'])
-        if results['next']:
-            if sent_requests > 150:
-                sent_requests = 0
-                print('Cooldown')
-                time.sleep(30)
-            results = sp.next(results)
-        else:
-            break
+# Obtém todos os álbuns
+albums = []
+with open('albums.json', 'w') as fp:
+    for artist in artists:
+        print("Obtendo álbuns...")
+        results = sp.artist_albums(artist['id'], album_type='album', limit=50)
+        sent_requests += 1
+        while True:
+            albums.extend(results['items'])
+            json.dump(results['items'], fp)   # Salva resultado parcial
+            if results['next']:
+                if sent_requests > 200:
+                    sent_requests = 0
+                    print('Cooldown')
+                    time.sleep(30)
+                results = sp.next(results)
+            else:
+                break
 
-# Salva detalhes dos álbuns
+# Subtitui resultados parciais com resultados finais
 with open('albums.json', 'w') as fp:
     json.dump(albums, fp)
 
-for album in albums:
-    print("Obtendo faixas dos álbuns...")
-    results = sp.album_tracks(album['id'], limit=50)
-    sent_requests += 1
-    while True:
-        album_tracks.extend(results['items'])
-        if results['next']:
-            if sent_requests > 150:
-                sent_requests = 0
-                print('Cooldown')
-                time.sleep(30)
-            results = sp.next(results)
-        else:
-            break
+# Obtém todas as faixas dos álbuns
+album_tracks = []
+with open('album_tracks.json', 'w') as fp:
+    for album in albums:
+        print("Obtendo faixas dos álbuns...")
+        results = sp.album_tracks(album['id'], limit=50)
+        sent_requests += 1
+        while True:
+            album_tracks.extend(results['items'])
+            json.dump(results['items'], fp)   # Salva resultado parcial
+            if results['next']:
+                if sent_requests > 200:
+                    sent_requests = 0
+                    print('Cooldown')
+                    time.sleep(30)
+                results = sp.next(results)
+            else:
+                break
 
-# Salva detalhes das faixas dos álbuns
+# Subtitui resultados parciais com resultados finais
 with open('album_tracks.json', 'w') as fp:
     json.dump(album_tracks, fp)
 
 # Compila lista com ID das faixas
+track_ids = []
 for track in album_tracks:
     track_ids.append(track['id'])
 
-for i in range(0, len(track_ids), 50):
-    if sent_requests > 150:
-        sent_requests = 0
-        print('Cooldown')
-        time.sleep(30)
+# Obtém detalhes de todas as faixas
+tracks = []
+with open('tracks.json', 'w') as fp:
+    for i in range(0, len(track_ids), 50):
+        if sent_requests > 200:
+            sent_requests = 0
+            print('Cooldown')
+            time.sleep(30)
 
-    print("Obtendo detalhes das faixas...")
-    results = sp.tracks(track_ids[i:i+50])
-    sent_requests += 1
-    tracks.extend(results['tracks'])
+        print("Obtendo detalhes das faixas...")
+        results = sp.tracks(track_ids[i:i+50])
+        sent_requests += 1
+        tracks.extend(results['tracks'])
+        json.dump(results['tracks'], fp)   # Salva resultado parcial
 
-# Salva detalhes de cada faixa
+# Subtitui resultados parciais com resultados finais
 with open('tracks.json', 'w') as fp:
     json.dump(tracks, fp)
