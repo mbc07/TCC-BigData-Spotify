2b611224140eaf44f83c5bbba32aeaeb517b9ea1
Isadora <139919282+isandri@users.noreply.github.com>
2024-08-11 18:08:51 -0400

    Update script.py

--- spotipy/script.py
+++ spotipy/script.py
@@ -3,12 +3,33 @@ import pandas as pd
 import hashlib
 import os
 
-# Essa função gera uma hash hexadeciamel para value
+def save_dataframes_to_json(dataframes, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for df_name, df in dataframes.items():
+        file_path = os.path.join(output_dir, f'{df_name}.json')
+        with open(file_path, 'w') as file:
+            file.write(df.to_json(orient='records', lines=False))
+
+def get_prefix(file_name):
+    if file_name.startswith("album_tracks_playlist_"):
+        return "album_tracks_playlist"
+    elif file_name.startswith("albums_playlist_"):
+        return "album_playlist"
+    elif file_name.startswith("features_album_tracks_playlist_"):
+        return "features_album_tracks_playlist"
+    elif file_name.startswith("features_playlist_"):
+        return "features_playlist"
+    elif file_name.startswith("playlist_"):
+        return "playlist"
+    else:
+        return None
+
 def generate_id(value):
     return hashlib.md5(str(value).encode()).hexdigest()
 
-# Essa função retorna os dados de um arquivo -- NÃO TRANSFORMA EM DATAFRAME MAIS AQUI
-def json_to_dataframe(file_path):
+def data_from_json(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     
@@ -19,7 +40,6 @@ def json_to_dataframe(file_path):
     
     return data
 
-# Esta função faz a divisão entre dados que são lista/dicionário com outros tipos de dados
 def extract_data(data, parent_key=''):
     strings = {}
     lists_dicts = {}
@@ -34,7 +54,6 @@ def extract_data(data, parent_key=''):
 
     return strings, lists_dicts
 
-# Esta função trata casos de listas e dicionários recursivamente, retornando os novos dataframes e os ids
 def process_lists_dicts(lists_dicts, prefix=''):
     dataframes = {}
     references = {}
@@ -43,89 +62,118 @@ def process_lists_dicts(lists_dicts, prefix=''):
         full_key = f"{prefix}.{key}" if prefix else key
 
         if isinstance(value, list):
-            df_list = []
-            ids_list = []
-            for item in value:
-                item_id = generate_id(item)
-                ids_list.append(item_id)
-                if isinstance(item, dict):
+            if all(isinstance(item, dict) for item in value):
+                df_list = []
+                ids_list = []
+                for item in value:
+                    item_id = item.get('id', generate_id(item))
+                    ids_list.append(item_id)
+
                     strings, sub_lists_dicts = extract_data(item)
-                    strings['ID'] = item_id
+                    strings['id'] = item_id
                     df_list.append(strings)
-                    sub_dfs, sub_refs = process_lists_dicts(sub_lists_dicts, full_key)
-                    dataframes.update(sub_dfs)
-                    references.update(sub_refs)
-                else:
-                    df_list.append({full_key: item, 'ID': item_id})
-            if df_list:
-                dataframes[full_key] = pd.DataFrame(df_list)
-            references[full_key] = ids_list
+
+                    if sub_lists_dicts:
+                        sub_dfs, sub_refs = process_lists_dicts(sub_lists_dicts, full_key)
+                        dataframes.update(sub_dfs)
+                        references.update(sub_refs)
+
+                if df_list:
+                    dataframes[full_key] = pd.DataFrame(df_list)
+                references[full_key] = ids_list
+            else:
+                item_id = generate_id(value)
+                df_list = [{key: value, 'id': item_id}]
+
+                if df_list:
+                    dataframes[full_key] = pd.DataFrame(df_list)
+                references[full_key] = item_id
 
         elif isinstance(value, dict):
-            item_id = generate_id(value)
+            item_id = value.get('id', generate_id(value))
+
             strings, sub_lists_dicts = extract_data(value)
-            strings['ID'] = item_id
+            strings['id'] = item_id
             dataframes[full_key] = pd.DataFrame([strings])
-            sub_dfs, sub_refs = process_lists_dicts(sub_lists_dicts, full_key)
-            dataframes.update(sub_dfs)
-            references.update(sub_refs)
+
+            if sub_lists_dicts:
+                sub_dfs, sub_refs = process_lists_dicts(sub_lists_dicts, full_key)
+                dataframes.update(sub_dfs)
+                references.update(sub_refs)
             references[full_key] = item_id
     
     return dataframes, references
 
 def unpack_response(directory_path):
-    all_files_data = []
+    all_files_data = {}
+    dataframes = {}
+    child_dfs = {}
 
     # Iterar sobre todos os arquivos JSON no diretório
     for file_name in os.listdir(directory_path):
         if file_name != 'playlists.json':
-            file_path = os.path.join(directory_path, file_name)
-            data = json_to_dataframe(file_path=file_path)
-            all_files_data.extend(data)
+            prefix = get_prefix(file_name)
+            if prefix:
+                file_path = os.path.join(directory_path, file_name)
+                data = data_from_json(file_path=file_path)
 
-    main_df_list = []
-    child_dfs = {}
-    for file_data in all_files_data: 
-        strings, lists_dicts = extract_data(file_data)
-        sub_dfs, references = process_lists_dicts(lists_dicts)
-   
-        # Substituir listas e dicionários por IDs correspondentes
-        for key, ids in references.items():
-            key_name = key.split('.')[-1]
-            if isinstance(ids, list):
-                strings[f'{key_name}_ids'] = ids
-            else:
-                strings[f'{key_name}_id'] = ids
+                if prefix in ['album_tracks_playlist', 'playlist']:
+                    genre = file_name.split('_')[-1].replace('.json', '')
+                    for item in data:
+                        if item is not None:
+                            item['genre'] = genre
 
-        main_df_list.append(strings)
-        
-        # Mesclar DataFrames filhos
-        for key, df in sub_dfs.items():
-            df_name = f"df_{key.split('.')[-1]}"
-            if df_name in child_dfs:
-                child_dfs[df_name] = pd.concat([child_dfs[df_name], df], ignore_index=True)
-            else:
-                child_dfs[df_name] = df
+                if prefix not in all_files_data:
+                    all_files_data[prefix] = []
+                all_files_data[prefix].extend(data)
 
-    # Criando o DataFrame principal com as strings
-    main_df = pd.DataFrame(main_df_list)
+    for prefix, data_list in all_files_data.items():
+        main_df_list = []
+        for file_data in data_list:
+            strings, lists_dicts = extract_data(file_data)
+            sub_dfs, references = process_lists_dicts(lists_dicts)
 
-    # Definindo DataFrames como variáveis globais
-    globals().update(child_dfs)
+            # Substituir listas e dicionários por IDs correspondentes
+            for key, ids in references.items():
+                key_name = key.split('.')[-1]
+                if isinstance(ids, list):
+                    strings[f'{key_name}_ids'] = ids
+                else:
+                    strings[f'{key_name}_id'] = ids
 
-    return main_df, child_dfs
+            main_df_list.append(strings)
+
+            # Mesclar DataFrames filhos
+            for key, df in sub_dfs.items():
+                df_name = f"{prefix}_{key.split('.')[-1]}"
+                if df_name in child_dfs:
+                    child_dfs[df_name] = pd.concat([child_dfs[df_name], df], ignore_index=True)
+                else:
+                    child_dfs[df_name] = df
+
+        # Criar DataFrame principal para o prefixo atual
+        main_df = pd.DataFrame(main_df_list)
+        dataframes[f'df_{prefix}'] = main_df
+
+        for df_name, df in dataframes.items():
+            df.dropna(subset=['id'], inplace=True)
+            df.drop_duplicates(subset=['id'], inplace=True)
+
+        for df_name, df in child_dfs.items():
+            df.dropna(subset=['id'], inplace=True)
+            df.drop_duplicates(subset=['id'], inplace=True)
+
+    return dataframes, child_dfs
 
 # MAIN
 
 directory_path = 'playlist-features-sample'
-main_df, child_dfs = unpack_response(directory_path=directory_path)
+main_dfs, child_dfs = unpack_response(directory_path=directory_path)
 
-# Exibindo DataFrames gerados
-print("Main DataFrame:")
-print(main_df, end="\n\n")
+# Salvar DataFrames principais em 'output-main'
+save_dataframes_to_json(main_dfs, output_dir='output-main-2')
 
-for df_name, df in child_dfs.items():
-    print(f"DataFrame {df_name}:")
-    print(df, end="\n\n")
+# Salvar DataFrames filhos em 'output-child'
+save_dataframes_to_json(child_dfs, output_dir='output-child-2')
 
-print(main_df.columns)
+print("DataFrames have been saved to JSON files.")
