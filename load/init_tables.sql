32e9dca8112908fd4d784a3cf3b517097434a992
Mateus B. Cassiano <mbc07@live.com>
2024-11-16 17:05:35 -0400

    Correções na conversão JSON para SQL, adicionado amostra de dados SQL

--- load/init_tables.sql
+++ load/init_tables.sql
@@ -1,27 +1,25 @@
-CREATE TABLE df_album (
+CREATE TABLE album (
     album_id TEXT PRIMARY KEY,
     album_name TEXT NOT NULL,
     release_date DATE
 );
 
-CREATE TABLE df_artists (
+CREATE TABLE artists (
     artist_id TEXT PRIMARY KEY,
     artist_name TEXT NOT NULL
 );
 
-CREATE TABLE df_images (
+CREATE TABLE images (
     image_id TEXT PRIMARY KEY,
     height INTEGER DEFAULT 0,
     width INTEGER DEFAULT 0,
     url TEXT NOT NULL
 );
 
-CREATE TABLE df_tracks (
+CREATE TABLE tracks (
     track_id TEXT PRIMARY KEY,
     track_name TEXT NOT NULL,
-    album_id TEXT REFERENCES df_album(album_id),
-    artist_id TEXT REFERENCES df_artists(artist_id),
-    image_id TEXT REFERENCES df_images(image_id),
+    album_id TEXT REFERENCES album(album_id),
     genre TEXT NOT NULL,
     explicit BOOLEAN DEFAULT false,
     duration_ms INTEGER DEFAULT 0,
@@ -35,17 +33,19 @@ CREATE TABLE df_tracks (
     acousticness REAL DEFAULT 0,
     instrumentalness REAL DEFAULT 0,
     liveness REAL DEFAULT 0,
-    valence REAL DEFAULT 0
+    valence REAL DEFAULT 0,
+    tempo REAL DEFAULT 0,
+    time_signature INTEGER DEFAULT 0
 );
 
-CREATE TABLE df_artist_track (
-    track_id TEXT REFERENCES df_tracks(track_id),
-    artist_id TEXT REFERENCES df_artists(artist_id),
+CREATE TABLE artist_track (
+    track_id TEXT REFERENCES tracks(track_id),
+    artist_id TEXT REFERENCES artists(artist_id),
     PRIMARY KEY (track_id, artist_id)
 );
 
-CREATE TABLE df_image_track (
-    track_id TEXT REFERENCES df_tracks(track_id),
-    image_id TEXT REFERENCES df_images(image_id),
+CREATE TABLE image_track (
+    track_id TEXT REFERENCES tracks(track_id),
+    image_id TEXT REFERENCES images(image_id),
     PRIMARY KEY (track_id, image_id)
 );
--- load/json2sql.py
+++ load/json2sql.py
@@ -4,24 +4,31 @@ import os
 import re
 import sys
 
+
+#==================================================================================================
+#-------------------------------------------- FUNÇÕES ---------------------------------------------
+
+# Deserializa dados de um arquivo JSON para uma variável
 def load_json(path):
     result = []
     with open(path, 'r', encoding='utf8') as file:
         result = json.load(file)
     return result
 
-def save_sql(data, path):
+# Serializa elementos de uma lista para um arquivo local
+def save_list(data, path):
     with open(path, 'w', encoding='utf8') as file:
         for item in data:
             file.write(item + '\n')
 
+# Converte objetos JSON para comandos SQL equivalentes
 def json_to_sql(data, table_name):
     sql_commands = []
     has_date_column = False
 
     if isinstance(data, list) and all(isinstance(item, dict) for item in data):
         columns = data[0].keys()
-        
+
         if args.verbose:
             print("Processando {} colunas de {} itens...".format(len(columns), len(data)))
 
@@ -35,14 +42,14 @@ def json_to_sql(data, table_name):
                     values.append(f"normalize_date('{str(value).replace("'", "''")}')")
                 else:
                     values.append(f"'{str(value).replace("'", "''")}'")
-            
+
             sql = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join(values)});"
             sql_commands.append(sql)
-    
+
     if has_date_column:
         if args.verbose:
             print("Coluna de tipo DATE detectada, adicionando função normalize_date() no SQL...")
-            
+
         sql_normalize_date = []
         sql_normalize_date.append("CREATE OR REPLACE FUNCTION normalize_date(partial_date TEXT)")
         sql_normalize_date.append("RETURNS DATE AS $$")
@@ -57,10 +64,10 @@ def json_to_sql(data, table_name):
         sql_normalize_date.append("END;")
         sql_normalize_date.append("$$ LANGUAGE plpgsql;")
         sql_normalize_date.append("")
-        
+
         sql_normalize_date.extend(sql_commands)
         return sql_normalize_date
-        
+
     else:
         return sql_commands
 
@@ -68,7 +75,7 @@ def json_to_sql(data, table_name):
 #==================================================================================================
 #----------------------------------- PROGRAMA PRINCIPAL (MAIN) ------------------------------------
 
-parser = argparse.ArgumentParser(allow_abbrev=False, description='Json2Sql [v1.1]', epilog='Converte dados de arquivos JSON em comandos SQL')
+parser = argparse.ArgumentParser(allow_abbrev=False, description='Json2Sql [v1.2]', epilog='Converte dados de arquivos JSON em comandos SQL')
 
 parser.add_argument('-i', '--in-dir', metavar=('DIR'), default='.', help='local contendo o(s) arquivo(s) a serem processados (padrão: pasta atual)')
 parser.add_argument('-o', '--out-dir', metavar=('DIR'), default='.', help='local onde o(s) arquivo(s) gerado(s) serão salvos (padrão: pasta atual)')
@@ -81,13 +88,14 @@ args = parser.parse_args()
 args.in_dir = os.path.abspath(args.in_dir)
 args.out_dir = os.path.abspath(args.out_dir)
 
+# Processamento da opção --parse-jsons (-p)
 if args.parse_jsons:
     for file in args.parse_jsons:
         source = load_json(os.path.join(args.in_dir, file))
-        table_name = re.sub('\\.[jJ][sS][oO][nN]$', '', os.path.basename(file))
+        table_name = re.sub('^[dD][fF]_', '', re.sub('\\.[jJ][sS][oO][nN]$', '', os.path.basename(file)))
         sql_commands = json_to_sql(source, table_name)
-        save_sql(sql_commands, os.path.join(args.out_dir, '{}.sql'.format(table_name)))
-        print('{} convertido para {} com sucesso'.format(os.path.basename(file), '{}.sql'.format(table_name)))
+        save_list(sql_commands, os.path.join(args.out_dir, '{}.sql'.format('pg_' + table_name)))
+        print('{} convertido para {} com sucesso'.format(os.path.basename(file), '{}.sql'.format('pg_' + table_name)))
 
 else:
     parser.print_help()
